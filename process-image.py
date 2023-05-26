4bd702db4221d35705da3c450daca89b5836e4f3
Al Udell <63697253+victorypoint@users.noreply.github.com>
2023-05-26 10:04:51 -0300

    Add files via upload

--- process-image.py
+++ process-image.py
@@ -94,8 +94,8 @@ inv = cv2.bitwise_not(closing)
 averageBlur = cv2.blur(inv, (3, 3))
 
 # OCR image
-ocr = PaddleOCR(lang='en', use_gpu=False, enable_mkldnn=True, show_log=False)
-result = ocr.ocr(averageBlur, cls=False, det=True, rec=True)
+ocr = PaddleOCR(lang='en', use_gpu=False, enable_mkldnn=True, use_angle_cls=False, table=False, layout=False, show_log=False, use_xpu=True, use_npu=True)
+result = ocr.ocr(zwiftImage, cls=False, det=True, rec=True)
 
 # Extract OCR text
 ocr_text = ''
